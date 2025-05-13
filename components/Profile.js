2d74fc08eda68db1a6d754493c3e429115e0759b
summer <summerwarren123@gmail.com>
2025-05-13 05:44:52 -0400

    .

--- components/Profile.js
+++ components/Profile.js
@@ -1,39 +1,73 @@
 export default {
     name: 'profile',
     props: {
-        showMyProfile: Boolean
+        showMyProfile: Boolean,
+        object: Object
+
+    },
+    data() {    
+        return {
+            value: null,
+            options: [
+                'Note Sharer',
+                'Active',
+                'Study Buddy'
+            ],
+            editProfile: {
+                bio: '',
+                tags: []
+            },
+        }
+
+    },
+    mounted() {
+        if (this.$graffitiSession?.value && this.object){
+            this.editProfile.bio = this.object.object.value.bio;
+            this.editProfile.tags = this.object.object.value.tags;
+        }
+
+    },
+    methods: {
+        async saveProfile(session, object) {
+            await this.$graffiti.patch(
+                {
+                    value: [{ "op": "replace", "path": "/bio", "value": this.editProfile.bio },
+                        { "op": "replace", "path": "/tags", "value": this.editProfile.tags },
+                    ],
+                    
+                },
+                object.url,
+                session,
+            );
+        }
     },
     template:`
-        <div class="overlay" v-if="showMyProfile">
-            <div class="profile-popup">
-                <div class="profile-popup-header">
-                    <button @click="$emit('close')">Close</button>
-                    <h2>Edit Profile</h2>
-                    <button @click="saveProfile($graffitiSession)">Save</button>
-                </div>
-                <div class="profile-popup-body">
-                    <div class="profile-input">
-                        <input id="username" type="text" :value="$graffitiSession.value.actor" readonly />
-                        <label for="username">Username</label>
-                    </div>
-                    <div class="profile-input">
-                        <input id="displayName" type="text" :value="$graffitiSession.value.actor" />
-                        <label for="displayname">Display Name</label>
-                    </div>
-                    <div class="toggle">
-                        <span>Student</span>
-                        <label class="switch">
-                            <input type="checkbox">
-                            <span class="slider"></span>
-                        </label>
-                        <span>Teacher</span>
-                    </div>
-                    <div class="profile-input">
-                        <textarea id="bio" maxlength="250"></textarea>
-                        <label for="Bio">Bio</label>
-                    </div>
-                </div>
+<div class="overlay" v-if="showMyProfile" @click.self="$emit('close')">
+    <div class="profile-popup">
+        <div class="profile-popup-header">
+            <button @click="$emit('close')">Close</button>
+            <h2>Edit Profile</h2>
+            <button @click="saveProfile($graffitiSession.value, object.object)">Save</button>
+        </div>
+        <div class="profile-popup-body">
+            <div class="profile-input">
+                <input id="username" type="text" :value="$graffitiSession.value.actor" readonly />
+                <label for="username">Username</label>
+            </div>
+            <div class="profile-input">
+                <textarea id="bio" maxlength="250" v-model="editProfile.bio"></textarea>
+                <label for="Bio">Bio</label>
+            </div>
+            <div class="profile-tag">
+                <v-select 
+                v-model='editProfile.tags'
+                :options="options"
+                placeholder="Select Tags"
+                :multiple="true"
+                ></v-select>
             </div>
         </div>
+    </div>
+</div>
     `
 }
--- index.js
+++ index.js
@@ -2,39 +2,60 @@ import { createApp, watchEffect } from "vue";
 import { GraffitiLocal } from "@graffiti-garden/implementation-local";
 import { GraffitiRemote } from "@graffiti-garden/implementation-remote";
 import { GraffitiPlugin } from "@graffiti-garden/wrapper-vue";
-import Header from "./components/Header.js";
-import NeonButton from "./components/NeonButton.js";
 import Profile from "./components/Profile.js";
+import MultiSelect from "vue-multiselect";
 
 const app = createApp({
     data() {
         return {
-            channels: ["main"],
+            channels: ["chatbook-mainfeed"],
             profile: null,
             showMyProfile: false,
-            editProfile: {
-                username: '',
-                displayname: '', 
-                role: '',
-                bio: ''
-            },
             myMessage: '',
             showMessagePopUp: false,
+            showCreateChatPopUp: false,
+            chatOptionsActive: false,
+            editUrl: null,
+            editContent: "",
+            otherProfile: null,
+            chat: {
+                name:'',
+                about: '',
+                people: []
+            }
         };
     },
     methods: {
-        async saveProfile(session) {
-
+        async createChat(session) {
+            if (!this.chat.name || !this.chat.about) return;
+            this.chat.people.append(this.$graffitiSession.value.actor);
+            await this.$graffiti.put(
+                {
+                    value: {
+                        name: this.chat.name,
+                        about: this.chat.about,
+                        people: this.chat.people,
+                    },
+                    channels: this.channels,
+                },
+                session,
+            );
+            this.chat = {
+                name:'',
+                about: '',
+                people: []
+            }
         },
-
         async sendMessage(session) {
             if (!this.myMessage) return;
-
             await this.$graffiti.put(
                 {
                     value: {
                         content: this.myMessage,
                         published: Date.now(),
+                        isDeleted: false,
+                        isEdited: false,
+                        oldContent: ""
                     },
                     channels: this.channels,
                 },
@@ -42,6 +63,84 @@ const app = createApp({
             );
 
             this.myMessage = "";
+        },
+        async updateMessage(session, object, newMessage, isDeleted=false, isEdited=false) {
+            await this.$graffiti.patch(
+                {
+                    value: [{ "op": "replace", "path": "/content", "value": newMessage },
+                    { "op": "replace", "path": "/isDeleted", "value": isDeleted },
+                    { "op": "replace", "path": "/oldContent", "value": object.value.content },
+                    { "op": "replace", "path": "/isEdited", "value": isEdited }
+                    ],
+                    
+                },
+                object.url,
+                session,
+            );
+        },
+        async deleteMessage(session, object) {
+            await this.updateMessage(session, object, "Message was deleted.", true);
+        },
+        async undoDeleteMessage(session, object) {
+            await this.updateMessage(session, object, object.value.oldContent, false);
+        },
+        editMessage(object) {
+            this.editUrl = object.url;
+            this.editContent = object.value.content;
+        },
+        closeMessageEdit() {
+            this.editUrl = null;
+            this.editContent = "";
+        },
+        async saveMessageEdit(session, object) {
+            await this.updateMessage(session, object, this.editContent, false, true);
+            this.closeMessageEdit()
+        },
+        handleProfileClick(object) {
+            this.showMyProfile = this.$graffitiSession.value.actor === object.actor; 
+        },
+
+        convertDate(timestamp) {
+            const date = new Date(timestamp)
+            return date.toLocaleDateString('en-US', {
+                month: "long",
+                day: "numeric",
+                year: "numeric",
+                hour: "numeric",
+                minute: "2-digit",
+            });
+        },
+        async showOtherProfile(session, otherActor) {
+            const stream = await this.$graffiti.discover(
+                this.channels,
+                {
+                    value: {
+                        required: ['type', 'username', 'bio', 'tags'],
+                        properties: {
+                            type: { type: 'string' },
+                            username: { type: 'string'},
+                            bio: { type: 'string' },
+                            tags: {
+                                type: 'array',
+                                items: { type: 'string' }
+                            }
+                        }
+                    }
+                },
+                session,
+            )
+
+            // let otherProfile = null;
+            for await (const result of stream) {
+                console.log(result)
+                if (result.object.actor === otherActor) {
+                    this.otherProfile = result.object;
+                    break;
+                }
+            }
+        },
+        handleOtherProfileClose() {
+            this.otherProfile = null;
         }
     },
     async mounted() {
@@ -50,21 +149,15 @@ const app = createApp({
                 const stream = await this.$graffiti.discover(
                     this.channels,
                     {
-                        properties: {
-                            value: {
-                                required: ['activity', 'object'],
-                                properties: {
-                                    activity: { type: 'string' },
-                                    object: {
-                                        required: ['type', 'username', 'displayname', 'role', 'bio'],
-                                        properties: {
-                                            type: { type: 'string' },
-                                            username: { type: 'string'},
-                                            displayname: { type: 'string' },
-                                            role: { type: 'string' },
-                                            bio: { type: 'string' }
-                                        }
-                                    }
+                        value: {
+                            required: ['type', 'username', 'bio', 'tags'],
+                            properties: {
+                                type: { type: 'string' },
+                                username: { type: 'string'},
+                                bio: { type: 'string' },
+                                tags: {
+                                    type: 'array',
+                                    items: { type: 'string' }
                                 }
                             }
                         }
@@ -72,46 +165,70 @@ const app = createApp({
                     this.$graffitiSession.value,
                 )
 
-                const results = [];
+                let results = [];
                 for await (const result of stream) {
                     if (result.object.actor === this.$graffitiSession.value.actor) {
                         results.push(result);
                     }
                 }
+                
                 if (results.length === 0) {
                     this.showMyProfile = true;
-                    const profile = await this.$graffiti.put(
+                    await this.$graffiti.put(
                         {
                             value: {
-                                activity: 'Create',
-                                object: {
-                                    type: 'Profile',
-                                    username: this.$graffitiSession.value.actor,
-                                    displayname: this.$graffitiSession.value.actor,
-                                    role: '',
-                                    bio: ''
-                                }
+                                type: 'Profile',
+                                username: this.$graffitiSession.value.actor,
+                                bio: '',
+                                tags: []
+                            
                             },
                             channels: this.channels
                         },
                         this.$graffitiSession.value,
                     )
-                    this.profile = profile;
-                } else {
-                    this.profile = results[0];
-                }
+                    const stream = await this.$graffiti.discover(
+                        this.channels,
+                        {
+                            value: {
+                                required: ['type', 'username', 'bio', 'tags'],
+                                properties: {
+                                    type: { type: 'string' },
+                                    username: { type: 'string'},
+                                    bio: { type: 'string' },
+                                    tags: {
+                                        type: 'array',
+                                        items: { type: 'string' }
+                                    }
+                                }
+                            }
+                        },
+                        this.$graffitiSession.value,
+                    )
+
+                    results = [];
+                    for await (const result of stream) {
+                        if (result.object.actor === this.$graffitiSession.value.actor) {
+                            results.push(result);
+                        }
+                    }
+                } 
+                this.profile = results[0];
+                
             }
         });
     }
 });
 
 app.use(GraffitiPlugin, {
-    graffiti: new GraffitiLocal(),
-    // graffiti: new GraffitiRemote(),
+    // graffiti: new GraffitiLocal(),
+    graffiti: new GraffitiRemote(),
 });
 
-app.component('header-component', Header);
-app.component('neon-button', NeonButton);
 app.component('profile', Profile);
+app.component('v-select', MultiSelect);
+// app.component('vue-select', window['VueMultiselect'].default)
+// Vue.component('v-select', VueSelect.VueSelect);
+// app.component('v-select', VueSelect?);
 
 app.mount("#app");
